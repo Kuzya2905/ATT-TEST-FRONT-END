da2d9d92c0a50d639e99bac51e3f994a7ff43164
Константин Кузьмин <kuzmin2905@gmail.com>
2024-10-23 12:07:07 +0300

    fix(2)

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const burger = document.querySelector(".headerBurger");
 const aside = document.querySelector(".aside");
 const main = document.querySelector(".main");
 const closeBurger = document.querySelector(".closeBurgerImg");
+const navButtonsMobileScreen = document.querySelector(
+  ".navButtonsMobileScreen"
+);
 
 burger.addEventListener("click", () => {
   aside.classList.add("active");
+  navButtonsMobileScreen.style.display = "flex";
   closeBurger.style.display = "block";
   main.style.display = "none";
 });
 
 closeBurger.addEventListener("click", () => {
   aside.classList.remove("active");
+  navButtonsMobileScreen.style.display = "none";
   main.style.display = "block";
   closeBurger.style.display = "none";
 });
